a0275ea350440a5002cff677a612dadfef2b60bc
HaroonAfzal22 <haroonnafzal@gmail.com>
2024-07-20 18:59:01 +0500

    video player added

--- components/HomeOne/MainBanner.js
+++ components/HomeOne/MainBanner.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import FsLightbox from "fslightbox-react";
+
 const MainBanner = () => {
-  const [toggler, setToggler] = useState(false);
+  const [isVideoVisible, setIsVideoVisible] = useState(false);
+
+  const handleImageClick = () => {
+    setIsVideoVisible(true);
+  };
+
   return (
     <>
-       <FsLightbox
-        toggler={toggler}
-        sources={["https://www.youtube.com/watch?v=bk7McNUjWgw"]}
-      />
-
       <div className="main-banner-two">
         <div className="d-table">
           <div className="d-table-cell">
@@ -22,7 +22,7 @@ const MainBanner = () => {
                       data-aos-delay="100"
                       data-aos-duration="1200"
                     >
-                       LathiTech
+                      LathiTech
                     </h1>
 
                     <div
@@ -31,97 +31,48 @@ const MainBanner = () => {
                       data-aos-duration="1200"
                     >
                       <p>
-                      Empowering your business with cutting-edge native mobile and web applications. Our expertise in validation, design, and development ensures robust solutions tailored to drive growth and achieve your strategic objectives.
+                        Empowering your business with cutting-edge native mobile and web applications. Our expertise in validation, design, and development ensures robust solutions tailored to drive growth and achieve your strategic objectives.
                       </p>
                     </div>
 
-                    {/* <div
-                      className="banner-btn"
-                      data-aos="fade-up"
-                      data-aos-delay="300"
-                      data-aos-duration="1200"
-                    >
-                      <Link href="/about-us">
-                        <a className="default-btn-one">
-                          About Us
-                          <span></span>
-                        </a>
-                      </Link>
-
-                      <Link href="/about-us">
-                        <a className="default-btn" href="/contact">
-                          Contact Us
-                          <span></span>
-                        </a>
-                      </Link>
-                    </div> */}
-
-                    <div className="banner-btn">
+                    <div className="banner-btn" style={{ display: 'flex', alignItems: 'center' }}>
                       <Link href="/about-us">
                         <a className="default-btn-one">
                           About Us <span></span>
                         </a>
                       </Link>
-                      <Link href="/about-us">
-                        <a className="default-btn-one" href="/contact">
-                          Contact Us
-                          <span></span>
+
+                      <Link href="/contact" style={{ marginLeft: '20px' }}>
+                        <a className="default-btn-one">
+                          Contact Us <span></span>
                         </a>
                       </Link>
 
-                      <div
-                        onClick={() => setToggler(!toggler)}
-                        className="video-btn popup-youtube"
-                      >
-                        <i className="fa fa-play"></i>
-                        Watch Video
-                      </div>
+                      <img
+                        src="CustomImages/playButton .png"
+                        alt="image"
+                        className="main_banner"
+                        style={{ marginLeft: '20px', width: '200px', height: 'auto', cursor: 'pointer' }}
+                        onClick={handleImageClick}
+                      />
                     </div>
+
+                    {isVideoVisible && (
+                      <video width="320" height="240" controls autoPlay style={{ marginTop: '20px' }}>
+                        <source src="/videos/IntroVideo.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                      </video>
+                    )}
                   </div>
                 </div>
 
                 <div className="col-lg-6 col-md-12">
                   <div className="banner-image h">
-                    {/* Shape images */}
-                    {/* <img
-                      src="/images/home-two/home-two-shape1.png"
-                      alt="image"
-                      data-aos="fade-up"
-                      data-aos-delay="100"
-                      data-aos-duration="1200"
-                    />
-
-                    <img
-                      src="/images/home-two/home-two-shape2.png"
-                      alt="image"
-                      data-aos="fade-left"
-                      data-aos-delay="100"
-                      data-aos-duration="1200"
-                    />
-
-                    <img
-                      src="/images/home-two/home-two-shape3.png"
-                      alt="image"
-                      data-aos="fade-right"
-                      data-aos-delay="100"
-                      data-aos-duration="1200"
-                    />
-
-                    <img
-                      src="/images/home-two/home-two-shape4.png"
-                      alt="image"
-                      data-aos="fade-up"
-                      data-aos-delay="100"
-                      data-aos-duration="1200"
-                    /> */}
-
                     <img
                       src="/images/banner.png"
                       alt="image"
                       className="main_banner"
                     />
-
-                    {/* Main image */}
 
                     <img
                       src="/images/banner.png"
