9e161ed20fa905cd8a2f9a513a1f17045079ce3b
Haroon-org <haroon@zealtouch.org>
2025-01-17 07:59:19 +0500

    new logo added lathi tech

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d81e80358f84b7e4"; }
+/******/ 		__webpack_require__.h = function() { return "fa1a8ec066ce9d12"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
